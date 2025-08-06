9e2b5aacc34a4971ad30b35c86c477c36ba6cd39
Liza Chevalier <lizalc@pm.me>
2025-08-06 16:29:01 -0500

    after hooks

--- Tests.cs
+++ Tests.cs
@@ -3,10 +3,65 @@
 public class Tests
 {
     [Test]
-    public async Task Basic()
+    public void Basic()
     {
-        await Assert.That(TestContext.Configuration).IsNotNull();
-        var value = TestContext.Configuration.Get("MyKey1");
-        await Assert.That(value).IsEqualTo("MyValue1");
+        Console.WriteLine("This is a basic test");
+    }
+}
+
+public sealed class Hooks
+{
+    [AfterEvery(Test)]
+    public static void AfterTest(TestContext testContext)
+    {
+        Console.WriteLine("After every test");
+    }
+
+    [After(Class)]
+    public static void AfterTestClass()
+    {
+        Console.WriteLine("After test class");
+    }
+
+    [AfterEvery(Class)]
+    public static void AfterEveryTestClass()
+    {
+        Console.WriteLine("After every test class");
+    }
+
+    [After(Assembly)]
+    public static void AfterTestAssembly()
+    {
+        Console.WriteLine("After test assembly");
+    }
+
+    [AfterEvery(Assembly)]
+    public static void AfterEveryTestAssembly()
+    {
+        Console.WriteLine("After every test assembly");
+    }
+
+    [After(TestSession)]
+    public static void AfterTestSession()
+    {
+        Console.WriteLine("After test session");
+    }
+
+    [AfterEvery(TestSession)]
+    public static void AfterEveryTestSession()
+    {
+        Console.WriteLine("After every test session");
+    }
+
+    [After(TestDiscovery)]
+    public static void AfterTestDiscovery()
+    {
+        Console.WriteLine("After test discovery");
+    }
+
+    [AfterEvery(TestDiscovery)]
+    public static void AfterEveryTestDiscovery()
+    {
+        Console.WriteLine("After every test discovery");
     }
 }
