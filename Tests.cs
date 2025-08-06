9b93935b83067fb024a488c42c2e1fca884d7f78
Liza Chevalier <lizalc@pm.me>
2025-08-06 10:59:20 -0500

    initial commit, test configuration always null?

---
+++ Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace tunit_testconfiguration;
+
+public class Tests
+{
+    [Test]
+    public async Task Basic()
+    {
+        await Assert.That(TestContext.Configuration).IsNotNull();
+        var value = TestContext.Configuration.Get("MyKey1");
+        await Assert.That(value).IsEqualTo("MyValue1");
+    }
+}
